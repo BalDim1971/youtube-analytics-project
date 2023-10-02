1d94fff2f7c735e61cb483eabfb1e30d3cf8e781
BalashovDV <korochun@yandex.ru>
2023-10-02 15:09:55 +0300

    Балашов Домашняя работа 3 Изменения в классе 'Channel'. 1. Добавлен метод `__str__`, возвращающий название и ссылку на канал по шаблону `<название_канала> (<ссылка_на_канал>)` 2. Реализованы возможности складывать / вычитать / сравнивать два канала между собой. Операции идут с количеством подписчиков

--- src/channel.py
+++ src/channel.py
@@ -22,9 +22,57 @@ class Channel:
         self.title = self.channel['items'][0]['snippet']['title']
         self.video_count = self.channel['items'][0]['statistics']['videoCount']
         self.url = 'https://www.youtube.com/channel/' + self.__channel_id
-        self.subscriberCount = self.channel['items'][0]['statistics']['subscriberCount']
+        self.subscriberCount = int(self.channel['items'][0]['statistics']['subscriberCount'])
         self.viewCount = self.channel['items'][0]['statistics']['viewCount']
 
+    def __str__(self) -> str:
+        '''
+        Магический метод, возвращающий название и ссылку на канал по шаблону `<название_канала> (<ссылка_на_канал>)`
+        '''
+        return f'{self.title} ({self.url})'
+    
+    def __add__(self, other):
+        '''
+        Магический метод сложения по количеству подписчиков
+        '''
+        return self.subscriberCount + other.subscriberCount
+    
+    def __sub__(self, other):
+        '''
+        Магический метод вычитания по количеству подписчиков
+        '''
+        return self.subscriberCount - other.subscriberCount
+    
+    def __gt__(self, other):
+        '''
+        Магический метод сравнения > по количеству подписчиков
+        '''
+        return self.subscriberCount > other.subscriberCount
+    
+    def __ge__(self, other):
+        '''
+        Магический метод сравнения >= по количеству подписчиков
+        '''
+        return self.subscriberCount >= other.subscriberCount
+    
+    def __lt__(self, other):
+        '''
+        Магический метод сравнения < по количеству подписчиков
+        '''
+        return self.subscriberCount < other.subscriberCount
+    
+    def __le__(self, other):
+        '''
+        Магический метод сравнения <= по количеству подписчиков
+        '''
+        return self.subscriberCount <= other.subscriberCount
+    
+    def __eq__(self, other):
+        '''
+        Магический метод сравнения == по количеству подписчиков
+        '''
+        return self.subscriberCount == other.subscriberCount
+    
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
